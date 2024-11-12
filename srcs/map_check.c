a7100b89f3426148344dead93beac094fa7bfdc6
mtelek <marcitelek@gmail.com>
2024-11-13 00:10:57 +0100

    added player_szie/2 buffer to wall_check, not bugging into walls anymore

--- srcs/map_check.c
+++ srcs/map_check.c
@@ -6,9 +6,11 @@
 /*   By: mtelek <mtelek@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 23:52:00 by mtelek            #+#    #+#             */
-/*   Updated: 2024/11/11 21:34:03 by mtelek           ###   ########.fr       */
+/*   Updated: 2024/11/13 00:09:47 by mtelek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "../Headers/cub3D.h"
 
 #include "../Headers/cub3D.h"
 
@@ -18,32 +20,38 @@ int	is_wall(t_main *main, float new_px, float new_py)
 	int	curr_y;
 	int	new_x;
 	int	new_y;
+	int buffer = main->player_data->player_size / 2;
 
 	curr_x = (int)(main->player_data->px / main->map->mapS);
 	curr_y = (int)(main->player_data->py / main->map->mapS);
 	new_x = (int)(new_px / main->map->mapS);
 	new_y = (int)(new_py / main->map->mapS);
-	if (new_x < 0 || new_x >= main->map->mapX[new_y] || new_y < 0
-		|| new_y >= main->map->mapY)
+	if (new_x < 0 || new_x >= main->map->mapX[new_y] || new_y < 0 || new_y >= main->map->mapY)
 		return (1);
 	if (main->map->map[new_y][new_x] == '1')
 		return (1);
+	if (main->map->map[(int)((new_py - buffer) / main->map->mapS)][new_x] == '1' || // Check above
+		main->map->map[(int)((new_py + buffer) / main->map->mapS)][new_x] == '1' || // Check below
+		main->map->map[new_y][(int)((new_px - buffer) / main->map->mapS)] == '1' || // Check left
+		main->map->map[new_y][(int)((new_px + buffer) / main->map->mapS)] == '1')   // Check right
+		return (1);
 	if (curr_x != new_x && curr_y != new_y)
 	{
-		if (main->map->map[curr_y][new_x] == '1'
-			|| main->map->map[new_y][curr_x] == '1')
+		if (main->map->map[curr_y][new_x] == '1' || main->map->map[new_y][curr_x] == '1')
 			return (1);
 	}
+
 	if (curr_x != new_x && curr_y == new_y)
 	{
-		if ((curr_x < new_x && main->map->map[curr_y][curr_x + 1] == '1')
-			|| (curr_x > new_x && main->map->map[curr_y][curr_x - 1] == '1'))
+		if ((curr_x < new_x && main->map->map[curr_y][curr_x + 1] == '1') ||
+			(curr_x > new_x && main->map->map[curr_y][curr_x - 1] == '1'))
 			return (1);
 	}
+
 	if (curr_y != new_y && curr_x == new_x)
 	{
-		if ((curr_y < new_y && main->map->map[curr_y + 1][curr_x] == '1')
-			|| (curr_y > new_y && main->map->map[curr_y - 1][curr_x] == '1'))
+		if ((curr_y < new_y && main->map->map[curr_y + 1][curr_x] == '1') ||
+			(curr_y > new_y && main->map->map[curr_y - 1][curr_x] == '1'))
 			return (1);
 	}
 	return (0);
