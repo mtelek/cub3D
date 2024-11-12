bc75467418735d7c9a52215e1c2da06baccab069
Vbezhevets <valeriy.bezhevets@ukr.net>
2024-11-12 10:37:45 +0100

    norm

--- INIT/text.c
+++ INIT/text.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   text.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bvalerii <bvalerii@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/10 11:22:57 by bvalerii          #+#    #+#             */
+/*   Updated: 2024/11/12 10:33:52 by bvalerii         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../Headers/cub3D.h"
 #include <X11/Xlib.h>
 
@@ -14,36 +26,44 @@ int load_texture(void *mlx, t_text *text)
 	return (0);
 }
 
-int load_texture_and_reverse(void *mlx,  t_text *text, int y, int x)
+void copy_pix_line(t_text *text, char *temp_text, int y)
 {
-	char	*temp_tex;
-	int		byte;
+	int	x;
+	int	byte;
 
-	byte = 0;
+	x = text->w - 1;
+	while (x >= 0)
+	{
+		while (byte < (text->bpp / 8))
+		{
+			temp_text[y * text->line_l + (text->w - x - 1) * (text->bpp / 8) + byte]
+			= text->addr[y * text->line_l + x * (text->bpp / 8) + byte];
+			byte++;
+		}
+		byte = 0;
+		x--;
+	}
+}
+
+int load_texture_and_reverse(void *mlx, t_text *text)
+{
+	char	*temp_text;
+	int		y;
+
+	y = 0;
 	if (load_texture(mlx, text))
         return (1);
-	temp_tex = malloc((size_t)text->line_l * (size_t)text->h);
-	if (!temp_tex)
-		return (free(temp_tex), 1);
+	temp_text = malloc(text->line_l * text->h);
+	if (!temp_text)
+		return (free(temp_text), 1);
 	else
 		while (y < text->h)
 		{
-			while (x >= 0)
-			{
-				while (byte < (text->bpp / 8))
-				{
-						temp_tex[y * text->line_l + (text->w - x - 1) * (text->bpp / 8) + byte]
-					= text->addr[y * text->line_l + x * (text->bpp / 8) + byte];
-					byte++;
-				}
-				byte = 0;
-				x--;
-			}
+			copy_pix_line(text, temp_text, y);
 			y++;
-			x = text->w - 1;
 		}
-	ft_memcpy(text->addr, temp_tex, text->line_l * text->h);
-	free(temp_tex);
+	ft_memcpy(text->addr, temp_text, text->line_l * text->h);
+	free(temp_text);
 	return(0);
 }
 
@@ -51,9 +71,9 @@ int load_textures(t_main *main) // do return
 {
 	if (
 	load_texture(main->data->mlx_ptr, main->textures->no) ||
-	load_texture_and_reverse(main->data->mlx_ptr, main->textures->so, 0, TEXT_SIZE - 1)
+	load_texture_and_reverse(main->data->mlx_ptr, main->textures->so)
 	|| load_texture(main->data->mlx_ptr, main->textures->ea)
-	|| load_texture_and_reverse(main->data->mlx_ptr, main->textures->we, 0, TEXT_SIZE - 1))
+	|| load_texture_and_reverse(main->data->mlx_ptr, main->textures->we))
 		return (1);
 	return (0);
 }
